6bac035ff2908eb281f9477aed1d9f76739e8091
Praiwan-ch <63160256@go.buu.ac.th>
2022-01-06 16:27:45 +0700

    first commit

---
+++ css01.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>63160256 form01</title>
+</head>
+
+<body>
+    
+    <form name="register">
+        ชื่อจริง : 
+        <br>
+            <input type="text"/>
+        <br>
+        นามสกุล : 
+        <br>
+            <input type="text"/>
+        <br>
+
+        <br>
+        เพศ : 
+        <br> 
+            <input type="radio" name="sex" value="ชา่ย"/> ชาย
+        <br>
+            <input type="radio" name="sex" value="หญิง"/> หญิง
+        <br>
+
+        <br>
+        ภาษาที่ถนัด :
+        <br>
+            <input type="checkbox"/> HTML
+        <br>
+            <input type="checkbox"/> CSS 
+        <br>
+            <input type="checkbox"/> PHP
+        <br>
+
+        <br>
+        <input type="reset" value=ยกเลิก /> 
+        <input type="submit" value=บันทึก />
+
+
+    </form>
+</body>
+
+</html>
---
+++ css02.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>  
+    <title>63160256 form2</title>
+</head>
+
+<body>
+    <form name=info>
+        เรียนอยู่สาขา : <br>
+        <input type="text"/>
+        <br>
+        <br>
+
+        สัตว์เลี้ยงที่ชอบมากที่สุด : 
+        <select>
+            <option value=1>สิงโต
+            <option value=2>เสือ
+            <option value=3>แมว
+        </select>
+        <br>
+        <br>
+
+        ข้อเสนอแนะ : <br>
+        <textarea></textarea>
+        <br>
+        <br>
+
+        <input type="reset" value=ยกเลิก />
+        <input type="submit" value=บันทึก />
+    </form>
+
+</body>
+
+</html>
